8d95a0b08e5414610a84215b9a697e11ad8474fd
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-09-20 14:59:32 +0400

    Added saving player data

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class DataManager : MonoBehaviour
+{
+	private string _playerDataFilename = "data.bin";
+
+	#region Initialization
+	void Awake()
+	{
+		_playerDataFilename = Path.Combine(Application.persistentDataPath, _playerDataFilename);
+
+		InitPlayerData();
+	}
+	#endregion
+
+	#region Public Fields
+	public LevelsDataBase LevelsDataBase;
+	public PlayerData PlayerData;
+	[SerializeField]
+	private PlayerDataSO _defaultPlayerData;
+	#endregion
+
+	private void InitPlayerData()
+    {
+		BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream dataFile;
+        if (File.Exists(_playerDataFilename))
+        {
+			dataFile = File.OpenRead(_playerDataFilename);
+
+			PlayerData = (PlayerData)bf.Deserialize(dataFile);
+		} else
+        {
+			dataFile = File.Create(_playerDataFilename);
+
+			PlayerData = _defaultPlayerData.PD;
+			bf.Serialize(dataFile, PlayerData);
+		}
+    }
+}
---
+++ Assets/Scripts/PlayerDataSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDataSO : ScriptableObject
+{
+
+    public PlayerData PD;
+}
+
+[System.Serializable]
+public class PlayerData
+{
+    public int Hp;
+    public string kek;
+}
